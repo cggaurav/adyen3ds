bea4baaa97afdfd36bea24c1aa84e79f83e862f0
Gaurav C <cggaurav@gmail.com>
2020-03-02 19:32:08 +0800

    originKeys fix

--- server/node/api/originKeys.js
+++ server/node/api/originKeys.js
@@ -4,7 +4,15 @@ const getPostParameters = require('../utils/getPostParameters');
 const handleCallback = require('../utils/handleCallback');
 
 module.exports = (res, request) => {
-    const originDomains = [`${request.protocol}://${request.headers.host}`];
+    
+    let originDomains
+    // WARNING: Have better logic here @Gaurav
+    if (request.headers.host.includes('localhost')) {
+    	originDomains = [`${request.protocol}://${request.headers.host}`];
+    } else {
+    	originDomains = [`https://${request.headers.host}`];
+    }
+
     const params = getPostParameters('originKeys', { originDomains });
 
     post(params, (err, response, body) => handleCallback({ err, response, body }, res));
